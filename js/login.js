f67057e8ebed2faf1b1adb0925b2f3806d1bb151
Oskari Arponen <oskarar@metropolia.fi>
2021-11-25 22:28:10 +0200

    added styling to login form

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ const loginForm = document.getElementById("login_form");
 
 
 // Adding a submit eventlistener which registers the user to the database with the given credentials.
-registerUserForm.addEventListener("submit", async (evt) => {
+/* registerUserForm.addEventListener("submit", async (evt) => {
   evt.preventDefault();
   const data = serializeJson(registerUserForm);
 
@@ -25,7 +25,7 @@ registerUserForm.addEventListener("submit", async (evt) => {
   location.href = "index.html";
 });
 
-
+ */
 
 // Adding an submit eventListener which attempts to log the user in with the given credentials.
 loginForm.addEventListener("submit", async (evt) => {
