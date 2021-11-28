b64227dd2cb8dff4b6d285511b8e423e7fab4f69
Oskari Arponen <oskarar@metropolia.fi>
2021-11-28 12:34:26 +0200

    add backend call to logout

--- js/logout.js
+++ js/logout.js
@@ -1,11 +1,31 @@
 "use strict";
 
+const logout = document.getElementById("logout_button");
+
 // Logs out the user.
-const logout = () => {
-  console.log(`Logging out`);
-  sessionStorage.removeItem("token");
-  sessionStorage.removeItem("user");
-  alert("You have logged out.");
-  displayHomeView();
-  checkLoggedStatus();
-}
+logout.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  console.log("Logging out");
+
+  try {
+    const options = {
+      headers: {
+        Authorization: "Bearer " + sessionStorage.getItem('token'),
+      },
+    };
+
+    const response = await fetch(url + '/auth/logout', options);
+    const json = await response.json();
+    console.log(json);
+
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+    alert("You have logged out.");
+    displayHomeView();
+    checkLoggedStatus();
+
+  } catch (e) {
+    console.log(`Failed to logout: ${e.message}`);
+  }
+
+});
